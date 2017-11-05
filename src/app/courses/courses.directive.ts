382cdf44fe3416c1df2956d18ae8306f2c774bd5
Anderson Fernandes <aasf_86@hotmail.com>
2017-11-05 16:48:10 -0200

    host listener

--- src/app/courses/courses.directive.ts
+++ src/app/courses/courses.directive.ts
@@ -1,27 +1,43 @@
-import { Directive, ElementRef, Renderer, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  Renderer, 
+  Renderer2,
+  HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCourses]'
 })
 export class CoursesDirective {
 
-  constructor(private _elementRef:ElementRef, private _renderer:Renderer2) {     
-    //_renderer.addClass(_elementRef, 'alert alert-success');
+  constructor(private _elementRef:ElementRef, private _renderer:Renderer) {     
+    
+    //this code below dont is recomendaded
+    //_elementRef.nativeElement.className = 'alert alert-success';
+
+
+    //this code below is correct, it shoulda been insert on the file
+    _renderer.setElementAttribute(_elementRef.nativeElement, 'class', 'alert alert-success');
   }
 }
 
 @Directive({
-  selector: '[highight]'
+  selector: 'p[highight]'
 })
 export class HighightCoursesDirective {
 
-  constructor(private _elementRef:ElementRef, private _renderer:Renderer2) {     
-    
-    //this code below dont is recomendaded
-    _elementRef.nativeElement.className = 'alert alert-success';
-
-    //this code below is correct, it shoulda been insert on the file
-    //_renderer.addClass(_elementRef, 'alert alert-success');
+  @HostListener('mouseover')
+  onMouseOver(){    
+    this._renderer.setElementStyle(this._elementRef.nativeElement, 'background-color', 'red');
   }
+
+  @HostListener('mouseout')
+  onMouseOut(){    
+    this._renderer.setElementStyle(this._elementRef.nativeElement, 'background-color', 'white');
+  }
+
+  constructor(
+    private _elementRef:ElementRef, 
+    private _renderer:Renderer) { }
 }
 
