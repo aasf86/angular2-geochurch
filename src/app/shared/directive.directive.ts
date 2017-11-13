d1129c45c1a326e75b3eb5062498cf694cde3c01
Anderson Fernandes <aasf_86@hotmail.com>
2017-11-13 12:30:26 -0200

    ...

--- src/app/shared/directive.directive.ts
+++ src/app/shared/directive.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, Input } from '@angular/core';
 /*
 import * as $ from 'jquery';
 import * as bootstrap from 'bootstrap';
@@ -10,10 +10,15 @@ import * as popper from 'popper.js';
 })
 export class DataToggle {
 
+  @Input('data-toggle')
+  dataToggle:string;
+
   constructor(
     private _elementRef:ElementRef,
     private _renderer:Renderer
   ) { 
+    console.log(this._elementRef.nativeElement);
+    console.log(this.dataToggle);
     /*
     setTimeout(() => {      
       console.log($);
@@ -22,5 +27,9 @@ export class DataToggle {
       console.log($(this._elementRef.nativeElement));
     });    
     */
+  }
+  ngOnInit(){
+    console.log(this._elementRef.nativeElement);
+    console.log(this.dataToggle);    
   }
 }
